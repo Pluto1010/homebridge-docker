1b331ed53c66255f6fa47f29bf9e3e87e071fd3b
Dennis Schulz <dennis@port42.org>
2017-02-27 18:17:55 +0100

    wip

--- assets/run.sh
+++ assets/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 export PATH=/usr/local/lib/node_modules/homebridge-install/node_modules/.bin:$PATH
 
+rm -rf /var/run/avahi-daemon/*
+rm /var/run/dbus.pid
+
 sed -i "s/rlimit-nproc=3/#rlimit-nproc=3/" /etc/avahi/avahi-daemon.conf
 sed -i '/^use-ipv6=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
 sed -i '/^enable-dbus=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
