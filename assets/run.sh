d6d5d3575f30590281317bde64a1c61dcf16ec74
Dennis Schulz <dennis@port42.org>
2017-02-27 19:20:36 +0100

    wip

--- assets/run.sh
+++ assets/run.sh
@@ -5,9 +5,9 @@ rm -rf /var/run/avahi-daemon/*
 rm /var/run/dbus.pid
 
 sed -i "s/rlimit-nproc=3/#rlimit-nproc=3/" /etc/avahi/avahi-daemon.conf
-sed -i '/^use-ipv6=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
+#sed -i '/^use-ipv6=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
 #sed -i '/^enable-dbus=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
-sed -i '/^publish-aaaa-on-ipv4=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
+#sed -i '/^publish-aaaa-on-ipv4=/s/=.*/=no/' /etc/avahi/avahi-daemon.conf
 
 dbus-daemon --system
 avahi-daemon --debug -D
