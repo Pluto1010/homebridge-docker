26f23b009fa87c82cffe16a84657340862301068
Dennis Schulz <dennis@port42.org>
2017-02-27 07:01:42 +0100

    forward port 61991 to enable harmony hub discovery

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t homebridge .
-docker run -d --restart unless-stopped --net=host -m 128M -p 0.0.0.0:51826:51826 -p 0.0.0.0:5353:5353 homebridge
+docker run -d --restart unless-stopped --net=host -m 128M -p 0.0.0.0:51826:51826 -p 0.0.0.0:5353:5353 -p 0.0.0.0:61991:61991 homebridge
